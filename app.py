cdf14fc643a60b04d580bf52df4c7400508d1fce
galloro <galloro@google.com>
2025-04-03 16:40:51 +0000

    1st working example

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringForm, SubmitForm, IntegerForm
+from wtforms import StringField, SubmitField, IntegerField
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies_db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "secret-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 class Movie(db.Model):
@@ -16,7 +19,7 @@ class Movie(db.Model):
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stars = db.Column(db.Integer)
-    movie_id = db.Column(db.Integer, db.ForeignKey(movie.id))
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
 
 with app.app_context():
     db.create_all()
@@ -26,7 +29,7 @@ class SubmitMovie(FlaskForm):
     submit = SubmitField("Add Movie")
 
 class VoteMovie(FlaskForm):
-    value = IntegerForm("Rating")
+    value = IntegerField("Rating")
     submit = SubmitField("Rate Movie")
 
 
@@ -44,6 +47,9 @@ def addmovie():
     movie_form = SubmitMovie()
     if movie_form.validate_on_submit():
         movie = Movie(title = movie_form.title.data)
-    return render_template("addmovie.html", movie_form = movie_form)
+        db.session.add(movie)
+        db.session.commit()
+    return render_template("addmovie.html" , movie_form = movie_form )
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
